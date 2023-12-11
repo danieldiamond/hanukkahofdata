90febd82ef390c0d582f0c82d0e1e0391e0b9bbb
Daniel Diamond <daniel.diamond@vista.com>
2023-12-12 08:31:18 +1100

    Add query five (#4)

--- models/queries/base.sql
+++ models/queries/base.sql
@@ -3,4 +3,4 @@ SELECT
 FROM {{ ref('fact_order_item_create') }} f
 LEFT JOIN {{ ref('dim_order') }} o ON f.order_id = o.order_id
 LEFT JOIN {{ ref('dim_customer') }} c ON f.customer_id = c.customer_id
-LEFT JOIN {{ ref('dim_product') }} d ON f.product_id = d.product_id
+LEFT JOIN {{ ref('dim_product') }} p ON f.product_id = p.product_id
