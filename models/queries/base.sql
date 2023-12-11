4fb8a6617aa54c70bb3355dd78d90ddc46a54942
Daniel Diamond <daniel.diamond@vista.com>
2023-12-12 08:41:29 +1100

    Format SQL (#5)

--- models/queries/base.sql
+++ models/queries/base.sql
@@ -1,6 +1,12 @@
 SELECT
-    f.*
-FROM {{ ref('fact_order_item_create') }} f
-LEFT JOIN {{ ref('dim_order') }} o ON f.order_id = o.order_id
-LEFT JOIN {{ ref('dim_customer') }} c ON f.customer_id = c.customer_id
-LEFT JOIN {{ ref('dim_product') }} p ON f.product_id = p.product_id
+
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.phone,
+    p.description
+
+FROM {{ ref('fact_order_item_create') }} AS f
+LEFT JOIN {{ ref('dim_order') }} AS o ON f.order_id = o.order_id
+LEFT JOIN {{ ref('dim_customer') }} AS c ON f.customer_id = c.customer_id
+LEFT JOIN {{ ref('dim_product') }} AS p ON f.product_id = p.product_id
--- models/queries/one.sql
+++ models/queries/one.sql
@@ -1,4 +1,8 @@
 SELECT
-    name, phone
+
+    name,
+    phone
+
 FROM {{ ref('dim_customer') }}
+
 WHERE REPLACE(phone, '-', '') = name_to_number(last_name)
