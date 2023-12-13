81df96f8cbe19092ff65b64f56e5f2cfd4115aa7
Daniel Diamond <daniel.diamond@vista.com>
2023-12-13 11:16:31 +1100

    Query Six (#7)
    * Add query six
    
    * Clean up SQL

--- models/queries/six.sql
+++ models/queries/six.sql
@@ -4,16 +4,13 @@ SELECT
     c.first_name,
     c.last_name,
     c.phone,
-    count(*) AS total_order_items
+    SUM(f.unit_price) - SUM(p.wholesale_cost) AS profit
 
 FROM {{ ref('fact_order_item_create') }} AS f
-LEFT JOIN {{ ref('dim_order') }} AS o ON f.order_id = o.order_id
 LEFT JOIN {{ ref('dim_customer') }} AS c ON f.customer_id = c.customer_id
 LEFT JOIN {{ ref('dim_product') }} AS p ON f.product_id = p.product_id
 
-WHERE EXTRACT(HOUR FROM f.time_ordered) < 5
-    AND p.product_id LIKE 'BKY%'
-
 GROUP BY 1, 2, 3, 4
-ORDER BY total_order_items DESC
+HAVING profit < 0
+ORDER BY profit
 LIMIT 1
