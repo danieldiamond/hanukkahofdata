88a8c2acccea9c5e48c615c8cba58204b6072385
danieldiamond <daniel.diamond@vista.com>
2023-12-14 10:31:19 +1100

    Add dbt expectations package and tests

--- models/queries/four.sql
+++ models/queries/four.sql
@@ -1,13 +1,12 @@
 SELECT
 
-    c.customer_id,
+    f.customer_id,
     c.first_name,
     c.last_name,
     c.phone,
     count(*) AS total_order_items
 
 FROM {{ ref('fact_order_item_create') }} AS f
-LEFT JOIN {{ ref('dim_order') }} AS o ON f.order_id = o.order_id
 LEFT JOIN {{ ref('dim_customer') }} AS c ON f.customer_id = c.customer_id
 LEFT JOIN {{ ref('dim_product') }} AS p ON f.product_id = p.product_id
 
