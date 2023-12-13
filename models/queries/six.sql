fc3c461fc54933eb285ae43a3628cdba680da5d4
danieldiamond <daniel.diamond@vista.com>
2023-12-13 16:08:36 +1100

    Fix profix calculation

--- models/queries/six.sql
+++ models/queries/six.sql
@@ -4,7 +4,7 @@ SELECT
     c.first_name,
     c.last_name,
     c.phone,
-    SUM(f.unit_price) - SUM(p.wholesale_cost) AS profit
+    SUM(f.qty * (f.unit_price - p.wholesale_cost)) AS profit
 
 FROM {{ ref('fact_order_item_create') }} AS f
 LEFT JOIN {{ ref('dim_customer') }} AS c ON f.customer_id = c.customer_id
