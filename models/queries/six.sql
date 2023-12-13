c4dbbaaf10d5c1fc4dd5af61dbb9eed2df319082
danieldiamond <daniel.diamond@vista.com>
2023-12-13 11:15:52 +1100

    Add query six

---
+++ models/queries/six.sql
@@ -0,0 +1,16 @@
+SELECT
+
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.phone,
+    SUM(f.unit_price) - SUM(p.wholesale_cost) AS profit
+
+FROM {{ ref('fact_order_item_create') }} AS f
+LEFT JOIN {{ ref('dim_customer') }} AS c ON f.customer_id = c.customer_id
+LEFT JOIN {{ ref('dim_product') }} AS p ON f.product_id = p.product_id
+
+GROUP BY 1, 2, 3, 4
+HAVING profit < 0
+ORDER BY profit
+LIMIT 1
