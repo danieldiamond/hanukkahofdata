d275cebd9f6d784b2b2f6b0f397e419821525be7
danieldiamond <daniel.diamond@vista.com>
2023-12-15 14:09:32 +1100

    Update queries

--- models/queries/two.sql
+++ models/queries/two.sql
@@ -1,3 +1,12 @@
+WITH old_customers AS (
+    SELECT
+        customer_id,
+        MAX(YEAR(time_ordered)) AS max_time_ordered
+    FROM {{ ref('fact_order_item_create') }}
+    GROUP BY 1
+    HAVING max_time_ordered = 2017
+)
+
 SELECT DISTINCT
 
     f.customer_id,
@@ -12,8 +21,9 @@ JOIN {{ ref('dim_customer') }} AS c ON f.customer_id = c.customer_id
 JOIN {{ ref('dim_product') }} AS p ON f.product_id = p.product_id
 
 WHERE YEAR(o.time_ordered) = 2017
-    AND c.first_name like 'J%'
-    AND c.last_name like 'P%'
+    AND c.first_name like 'D%'
+    AND c.last_name like 'S%'
     AND (LOWER(p.description) like '%coffee%'
         OR LOWER(p.description) like '%bagel%'
     )
+    AND f.customer_id IN (SELECT customer_id FROM old_customers)
